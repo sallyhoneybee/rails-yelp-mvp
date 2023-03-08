3b6da24b584af7a0358598f56a41b67e6e5ce9ef
Summer Chew <summerchewjx@gmail.com>
2023-03-08 15:09:22 +0800

    created reviews new vview with working form and create method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,16 @@
 class ReviewsController < ApplicationController
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+
     if @review.save
-      redirect_to restaurant_reviews_path(@review)
+      redirect_to restaurant_path(@restaurant)
     else
       render :new, status: :unprocessable_entity
     end
