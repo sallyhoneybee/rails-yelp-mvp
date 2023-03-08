a6ad92748590f94293e86d4cea0184193cd26ecf
Summer Chew <summerchewjx@gmail.com>
2023-03-08 17:35:03 +0800

    refactor embed partial

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,6 +5,8 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
+    @reviews = @restaurant.reviews
   end
 
   def new
