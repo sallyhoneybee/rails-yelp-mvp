2aee66ff43ed59c4c39429e2cf1151241fa7498c
Summer Chew <summerchewjx@gmail.com>
2023-03-08 13:21:57 +0800

    created reviews controller with new and create methods. updated route with nested rerouces

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,20 @@
+class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to restaurant_reviews_path(@review)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
